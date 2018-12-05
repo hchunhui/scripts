e54bcba2d34bc69175c0c23c8e2918a16b2913a7
Chunhui He <hchunhui@mail.ustc.edu.cn>
2018-12-05 14:48:54 +0000

    update dnsp

--- ssocks/dnsp/dnsp.c
+++ ssocks/dnsp/dnsp.c
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 
 int sock_create(char *ip, unsigned short port)
 {
@@ -63,6 +64,8 @@ int main(int argc, char **argv)
 	dnsaddr.sin_addr.s_addr = inet_addr("8.8.8.8");
 	dnsaddr.sin_port = htons(53);
 
+	signal(SIGCHLD, SIG_IGN);
+
 	while(1) {
 		struct sockaddr_in peer_addr;
 		socklen_t slen = sizeof(struct sockaddr_in);
@@ -72,49 +75,53 @@ int main(int argc, char **argv)
 		if (n < 0)
 			break;
 
-		int fd2 = socket(AF_INET, SOCK_STREAM, 0);
-		if (fd2 < 0) {
-			perror("socket");
-			continue;
-		}
+		if (fork() == 0) {
+			int fd2 = socket(AF_INET, SOCK_STREAM, 0);
+			if (fd2 < 0) {
+				perror("socket");
+				exit(1);
+			}
 
-		ret = connect(fd2, (struct sockaddr *)&dnsaddr, sizeof(struct sockaddr_in));
-		if (ret < 0) {
-			perror("connect");
-			continue;
-		}
+			ret = connect(fd2, (struct sockaddr *)&dnsaddr, sizeof(struct sockaddr_in));
+			if (ret < 0) {
+				perror("connect");
+				close(fd2);
+				exit(1);
+			}
 
-		buf[0] = (n >> 8);
-		buf[1] = n & 0xff;
-		ret = write(fd2, buf, n + 2);
-		if (n + 2 != ret) {
-			perror("write");
+			buf[0] = (n >> 8);
+			buf[1] = n & 0xff;
+			ret = write(fd2, buf, n + 2);
+			if (n + 2 != ret) {
+				perror("write");
+				close(fd2);
+				exit(1);
+			}
+
+			n = readn(fd2, buf, 2);
+			if (n != 2) {
+				perror("read1");
+				close(fd2);
+				exit(1);
+			}
+
+			n = (buf[0] << 8) | buf[1];
+			if (n <= 0 || n > LEN) {
+				close(fd2);
+				exit(1);
+			}
+
+			ret = readn(fd2, buf, n);
+			if (n != ret) {
+				perror("read2");
+				close(fd2);
+				exit(1);
+			}
+
+			sendto(fd, buf, n, 0, (struct sockaddr *)&peer_addr, slen);
 			close(fd2);
-			continue;
+			exit(0);
 		}
-
-		n = readn(fd2, buf, 2);
-		if (n != 2) {
-			perror("read1");
-			close(fd2);
-			continue;
-		}
-
-		n = (buf[0] << 8) | buf[1];
-		if (n <= 0 || n > LEN) {
-			close(fd2);
-			continue;
-		}
-
-		ret = readn(fd2, buf, n);
-		if (n != ret) {
-			perror("read2");
-			close(fd2);
-			continue;
-		}
-
-		sendto(fd, buf, n, 0, (struct sockaddr *)&peer_addr, slen);
-		close(fd2);
 	}
 	close(fd);
 	return 0;
